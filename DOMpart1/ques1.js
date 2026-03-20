59919cfba89d0bc16dbe0a1effd3c76e11ce4131
Harshit Srivastav <harshitsrivastav@gmail.com>
2026-03-20 23:27:11 +0530

    Part-1 finished

---
+++ DOMpart1/ques1.js
@@ -0,0 +1,7 @@
+let x = document.querySelector("h2");
+
+// let y = " from Apna College students"
+// x = x.append(y);
+
+console.log(x.innerText);
+x.innerText = x.innerText + " from Apna College students"; // Concat
--- DOMpart1/script.js
+++ DOMpart1/script.js
@@ -1,6 +1,12 @@
-// let heading = document.getElementById("heading");  //h1
+// let heading = document.getElementById("heading1");  //h1
 // console.dir(heading);
 
-let headings = document.getElementsByClassName("headingClass");
-console.dir(headings);
-console.log(headings);
+// let headings = document.getElementsByClassName("headingClass");
+// console.dir(headings);
+// console.log(headings);
+
+let firstElements = document.querySelector("p");
+console.dir(firstElements);
+
+let allElements = document.querySelectorAll("p");
+console.dir(allElements);
